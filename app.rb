831280557e43374c021392e54fea98e66df3e1f7
blumj808 <146663030+blumj808@users.noreply.github.com>
2023-10-28 20:41:48 +0000

    save

--- app.rb
+++ app.rb
@@ -17,11 +17,11 @@ get("/") do
   raw_data_string = raw_data.to_s
  
   # convert the string to JSON
-  parsed_data = JSON.parse(raw_data_string)
+  @parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
-  @symbols = parsed_data.fetch("currencies")
-  @currencies = @symbols.keys
+  @symbols = @parsed_data.fetch("currencies")
+ # @currencies = @symbols.keys
 
   # render a view template where I show the symbols
   erb(:homepage)
@@ -33,7 +33,7 @@ get("/:from_currency") do
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["Exchange_Rate_Key"]}"
   raw_data = HTTP.get(apu_url)
   raw_data_string = raw_data.to_s
-  parsed_data = SON.parse(raw_data_string)
+  parsed_data = JSON.parse(raw_data_string)
   symbols = parsed_data.fetch("currencies")
   @currencies = symbols.keys
   erb(:from_currency)
